15d6470babe2fb4ccbcc447de06cc4a43f5044f5
KsyushaKI <oksanaakadem39@gmail.com>
2023-08-09 22:23:05 +0300

    change app.py

--- documents_merger/app.py
+++ documents_merger/app.py
@@ -47,8 +47,6 @@ def get_merged_excel():
     except Exception:
         flash('Что-то пошло не так', 'danger')
         return redirect(url_for('get_excel_hundler'))
-    finally:
-        os.remove(ce.path_to_output_file)
 
 
 @app.get('/pdf_hundler')
@@ -69,5 +67,3 @@ def get_merged_pdf():
     except Exception:
         flash('Что-то пошло не так', 'danger')
         return redirect(url_for('get_pdf_hundler'))
-    finally:
-        os.remove(cp.path_to_output_file)
